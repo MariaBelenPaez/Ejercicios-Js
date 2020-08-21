25d05ad22394b38b81fa54b3c016141e1847799f
María <belenpaez@live.com.ar>
2020-08-21 00:15:43 -0300

    ejerciciounojava

---
+++ index.js
@@ -0,0 +1,7 @@
+let respuestaUsuario = prompt("nombre ")
+let apellidoUsuario = prompt("apellido")
+
+let saludo = "Hola,"
+let saludos = " ,Bienvenida a Ada!!"
+
+alert(saludo + respuestaUsuario + apellidoUsuario + saludos)
